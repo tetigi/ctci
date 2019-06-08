8eafb7f318e23ec0f2e13fb08da230d0b39e78f2
Luke Tomlin <luke.tomlin@gmail.com>
2019-06-08 10:59:59 +0100

    More linked list goodness

--- src/linked_lists.py
+++ src/linked_lists.py
@@ -2,6 +2,14 @@ class Node:
     def __init__(self, value):
         self.value = value
 
+    def __len__(self):
+        count = 0
+        tmp = self
+        while tmp != None:
+            count += 1
+            tmp = tmp.next
+        return count
+
     def __repr__(self):
         if self.next:
             next = repr(self.next)
@@ -58,3 +66,41 @@ def delete_in_middle(pointer):
         next_node = next_node.next
 
     last.next = None
+
+def partition(x, xs):
+    left = xs
+    right = xs.next
+
+    while left.value < x:
+        left = left.next
+        right = right.next
+
+    while right:
+        if right.value < x:
+            tmp = left.value
+            left.value = right.value
+            right.value = tmp
+            left = left.next
+        right = right.next
+
+def sum_lists(n1, n2):
+    carry = 0
+    output = []
+
+    while n1 or n2:
+        n1_val = 0 if n1 == None else n1.value
+        n2_val = 0 if n2 == None else n2.value
+        res = n1_val + n2_val + carry
+        carry = 0
+        if res >= 10:
+            carry = 1
+            output.append(res % 10)
+        else:
+            output.append(res)
+        n1 = n1.next if n1 != None else None
+        n2 = n2.next if n2 != None else None
+
+    if carry == 1:
+        output.append(1)
+
+    return linked_list(output)
