3b3c0f522576e98d7ddb35d91ddae85d89c3c96d
Luke Tomlin <luke.tomlin@gmail.com>
2019-06-08 22:19:14 +0100

    More bits goodness

--- src/bits.rs
+++ src/bits.rs
@@ -6,6 +6,47 @@ fn insertion(n: u32, m: u32, i: u8, j: u8) -> u32 {
     (n & mask) | (m << i)
 }
 
+fn flip_bit(n: u32) -> usize {
+    let mut n = n;
+    let mut seqs = vec![];
+    let mut this_seq = vec![];
+    let mut this_ones_len = 0;
+    let mut seen_gap = true;
+
+    while n != 0 {
+        let bit = n & 1;
+        println!("bit is {}", bit);
+        if bit == 1 {
+            if seen_gap {
+                this_ones_len = 1;
+            } else {
+                this_ones_len += 1;
+            }
+            seen_gap = false;
+        } else {
+            if seen_gap {
+                println!("pushing seq {:?}", this_seq);
+                seqs.push(this_seq);
+                this_seq = vec![];
+            } else {
+                seen_gap = true;
+                this_seq.push(this_ones_len);
+            }
+        }
+
+        n = n >> 1;
+    }
+
+    this_seq.push(this_ones_len);
+    seqs.push(this_seq);
+
+    println!("{:?}", seqs);
+
+    seqs.iter().fold(0, |acc, xs| {
+        acc.max(xs.windows(2).fold(0, |acc, xs| acc.max(xs[0] + xs[1] + 1)))
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,5 +54,11 @@ mod tests {
     #[test]
     fn test_insertion() {
         assert_eq!(0b10001001100, insertion(0b10000000000, 0b10011, 2, 6));
+    }
+
+    #[test]
+    fn test_flip_bit() {
+        assert_eq!(8, flip_bit(1775));
+        assert_eq!(6, flip_bit(0b111011001101101));
     }
 }
